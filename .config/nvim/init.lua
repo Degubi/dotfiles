daf2fb182a693ef3dc63e7a2aff6de3677835682
Degubi <gergonoorbi@gmail.com>
2025-02-13 20:21:07 +0100

    Remove luasnip as I don't use it

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -188,30 +188,14 @@ require('lazy').setup({
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            {
-                'L3MON4D3/LuaSnip',
-                build = (function()
-                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                        return
-                    end
-                    return 'make install_jsregexp'
-                end)(),
-                dependencies = {}
-            },
-            'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-nvim-lsp-signature-help'
         },
         config = function()
             local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            luasnip.config.setup({})
 
             cmp.setup({
-                snippet = {
-                    expand = function(args) luasnip.lsp_expand(args.body) end
-                },
                 completion = {
                     autocomplete = false,
                     completeopt = 'menu,menuone,noinsert'
