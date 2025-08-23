85b698ece8e56b546626213b8cc762220c8d9c04
Degubi <gergonoorbi@gmail.com>
2025-08-23 22:27:12 +0200

    Small cleanups in neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,15 @@
 vim.g.mapleader = ' '
-vim.g.have_nerd_font = false
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.winborder = 'solid'
 vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
-vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
-vim.o.timeoutlen = 1000
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.list = true
@@ -23,10 +19,10 @@ vim.o.scrolloff = 5
 vim.o.sidescrolloff = 10
 vim.o.endofline = false
 vim.o.fixendofline = false
-vim.o.tabstop = 4
 vim.o.expandtab = true
-vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.wrap = false
 vim.o.statusline = table.concat({
     ' %f',
@@ -60,15 +56,12 @@ vim.lsp.config('jdtls', {
     settings = {
         java = {
             format = { insertSpaces = true },
-            sources = {
-                organizeImports = { starThreshold = 1, staticStarThreshold = 1 }
-            }
+            sources = { organizeImports = { starThreshold = 1, staticStarThreshold = 1 }}
         }
     }
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = '*',
     callback = function()
         local cursor = vim.fn.getpos('.')
         vim.cmd([[%s/\s\+$//e]])
@@ -202,10 +195,7 @@ require('lazy').setup({
                 colors.bg_float = '#222228'
             end
         },
-        init = function()
-            vim.cmd.colorscheme('tokyonight-night')
-            vim.cmd.hi('Comment gui=none')
-        end
+        init = function() vim.cmd.colorscheme('tokyonight-night') end
     },
     {
         'nvim-treesitter/nvim-treesitter',
