85a13f944a5e4403d42d354939dce5afc203bbe9
Degubi <gergonoorbi@gmail.com>
2025-09-14 16:46:30 +0200

    Prepare neovim config for 0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,7 +34,63 @@ vim.o.statusline = table.concat({
     ('%%#PmenuSel#[%s] %%*'):format('%{&fileformat}'),
     ('%%#Search#%s%%*'):format(' %l:%c ')
 })
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath })
+end
+vim.o.rtp = lazypath .. ',' .. vim.o.rtp
+
+require('lazy').setup({
+    { 'nvim-lua/plenary.nvim' },
+    { 'folke/tokyonight.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'saghen/blink.cmp', version = '1.*' },
+    { 'neovim/nvim-lspconfig' },
+    { 'mason-org/mason.nvim' },
+    { 'ThePrimeagen/harpoon', branch = 'harpoon2' },
+    { 'j-hui/fidget.nvim' },
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }
+}, {
+    ui = {
+        icons = {
+            cmd = '⌘', config = '🛠', event = '📅', ft = '📂', init = '⚙',
+            keys = '🗝', plugin = '🔌', runtime = '💻', require = '🌙',
+            source = '📄', start = '🚀', task = '📌', lazy = '💤 '
+        }
+    }
+})
+
+require('tokyonight').setup({
+    styles = { comments = { italic = false }, keywords = { italic = false }},
+    on_colors = function(colors)
+        colors.bg = '#16161e'
+        colors.bg_statusline = '#292e42'
+        colors.bg_float = '#222228'
+    end
+})
+
 vim.cmd.syntax('off')
+vim.cmd.colorscheme('tokyonight-night')
+
+local telescope_main = require('telescope')
+telescope_main.setup({
+    extensions = {[ 'ui-select' ] = require('telescope.themes').get_dropdown()},
+    pickers = {
+        find_files = { previewer = false },
+        diagnostics = { previewer = false },
+        buffers = {
+            previewer = false,
+            mappings = { n = {[ 'dd' ] = 'delete_buffer' }}
+        }
+    }
+})
+telescope_main.load_extension('ui-select')
+
+local telescope = require('telescope.builtin')
+local harpoon = require('harpoon')
+harpoon.setup()
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
@@ -50,15 +106,33 @@ vim.keymap.del('n', 'grn')
 vim.keymap.del('n', 'grr')
 vim.keymap.del('n', 'gri')
 vim.keymap.del('n', 'gra')
+vim.keymap.set('n', '<leader>sf', telescope.find_files)
+vim.keymap.set('n', '<leader>sg', function() telescope.live_grep({ additional_args = { '--fixed-strings' }}) end)
+vim.keymap.set('n', '<leader>sd', telescope.diagnostics)
+vim.keymap.set('n', '<leader>sr', telescope.resume)
+vim.keymap.set('n', '<leader><leader>', telescope.buffers)
+vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
+vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
 
-vim.diagnostic.config({ virtual_text = true })
-vim.lsp.config('jdtls', {
-    settings = {
-        java = {
-            format = { insertSpaces = true },
-            sources = { organizeImports = { starThreshold = 1, staticStarThreshold = 1 }}
-        }
-    }
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+    callback = function(event)
+        local telescope = require('telescope.builtin')
+        vim.keymap.set('n', 'gd', telescope.lsp_definitions, { buffer = event.buf })
+        vim.keymap.set('n', 'gr', telescope.lsp_references, { buffer = event.buf })
+        vim.keymap.set('n', 'gI', telescope.lsp_implementations, { buffer = event.buf })
+        vim.keymap.set('n', '<leader>ss', telescope.lsp_document_symbols, { buffer = event.buf })
+        vim.keymap.set('n', '<leader>sw', telescope.lsp_dynamic_workspace_symbols, { buffer = event.buf })
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = event.buf })
+        vim.keymap.set({ 'n', 'v' }, '<leader>q', vim.lsp.buf.code_action, { buffer = event.buf })
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { buffer = event.buf })
+
+        vim.lsp.get_client_by_id(event.data.client_id).server_capabilities.semanticTokensProvider = nil
+    end
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
@@ -69,149 +143,48 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-    callback = function(event)
-        local builtin = require('telescope.builtin')
+vim.diagnostic.config({ virtual_text = true })
+vim.lsp.config('jdtls', { settings = { java = {
+    format = { insertSpaces = true },
+    sources = { organizeImports = { starThreshold = 1, staticStarThreshold = 1 }}
+}}})
 
-        vim.keymap.set('n', 'gd', builtin.lsp_definitions, { buffer = event.buf })
-        vim.keymap.set('n', 'gr', builtin.lsp_references, { buffer = event.buf })
-        vim.keymap.set('n', 'gI', builtin.lsp_implementations, { buffer = event.buf })
-        vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { buffer = event.buf })
-        vim.keymap.set('n', '<leader>sw', builtin.lsp_dynamic_workspace_symbols, { buffer = event.buf })
-        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = event.buf })
-        vim.keymap.set({ 'n', 'v' }, '<leader>q', vim.lsp.buf.code_action, { buffer = event.buf })
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { buffer = event.buf })
-
-        vim.lsp.get_client_by_id(event.data.client_id).server_capabilities.semanticTokensProvider = nil
-    end
+require('nvim-treesitter.configs').setup({
+    auto_install = true,
+    highlight = { enable = true, additional_vim_regex_highlighting = false }
 })
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath })
+require('fidget').setup()
+require('mason').setup()
+
+local registry = require('mason-registry')
+local package_to_lsp_names = {}
+
+for _, pkg_spec in ipairs(registry.get_all_package_specs()) do
+    package_to_lsp_names[pkg_spec.name] = vim.tbl_get(pkg_spec, 'neovim', 'lspconfig')
 end
-vim.o.rtp = lazypath .. ',' .. vim.o.rtp
 
-require('lazy').setup({
-    { 'nvim-lua/plenary.nvim' },
-    {
-        'nvim-telescope/telescope.nvim',
-        config = function()
-            local telescope = require('telescope')
+for _, package_name in ipairs(registry.get_installed_package_names()) do
+    vim.lsp.enable(package_to_lsp_names[package_name])
+end
 
-            telescope.setup({
-                extensions = {
-                    [ 'ui-select' ] = require('telescope.themes').get_dropdown()
-                },
-                pickers = {
-                    find_files = { previewer = false },
-                    diagnostics = { previewer = false },
-                    buffers = {
-                        previewer = false,
-                        mappings = { n = { [ 'dd' ] = 'delete_buffer' }}
-                    }
-                }
-            })
-
-            telescope.load_extension('ui-select')
-
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>sf', builtin.find_files)
-            vim.keymap.set('n', '<leader>sg', function() builtin.live_grep({ additional_args = { '--fixed-strings' }}) end)
-            vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
-            vim.keymap.set('n', '<leader>sr', builtin.resume)
-            vim.keymap.set('n', '<leader><leader>', builtin.buffers)
-        end
-    },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-    {
-        'saghen/blink.cmp',
-        version = '1.*',
-        opts = {
-            cmdline = { enabled = false },
-            sources = { default = { 'lsp', 'buffer' }},
-            signature = { enabled = true },
-            completion = {
-                accept = { auto_brackets = { enabled = false }},
-                documentation = { auto_show = true, auto_show_delay_ms = 0 },
-                list = { selection = { auto_insert = false }},
-                menu = {
-                    auto_show = false,
-                    max_height = 30,
-                    draw = { columns = {{ 'label', 'label_description', gap = 1 }, { 'kind' }}}
-                }
-            },
-            keymap = {
-                preset = 'default',
-                [ '<C-k>' ] = { 'select_prev', 'fallback' },
-                [ '<C-j>' ] = { 'select_next', 'fallback' }
-            }
+require('blink.cmp').setup({
+    cmdline = { enabled = false },
+    sources = { default = { 'lsp', 'buffer' }},
+    signature = { enabled = true },
+    completion = {
+        accept = { auto_brackets = { enabled = false }},
+        documentation = { auto_show = true, auto_show_delay_ms = 0 },
+        list = { selection = { auto_insert = false }},
+        menu = {
+            auto_show = false,
+            max_height = 30,
+            draw = { columns = {{ 'label', 'label_description', gap = 1 }, { 'kind' }}}
         }
     },
-    { 'neovim/nvim-lspconfig' },
-    {
-        'mason-org/mason.nvim',
-        config = function()
-            require('mason').setup()
-
-            local registry = require('mason-registry')
-            local package_to_lsp_names = {}
-
-            for _, pkg_spec in ipairs(registry.get_all_package_specs()) do
-                package_to_lsp_names[pkg_spec.name] = vim.tbl_get(pkg_spec, 'neovim', 'lspconfig')
-            end
-
-            for _, package_name in ipairs(registry.get_installed_package_names()) do
-                vim.lsp.enable(package_to_lsp_names[package_name])
-            end
-        end
-    },
-    {
-        'ThePrimeagen/harpoon',
-        branch = 'harpoon2',
-        config = function()
-            local harpoon = require('harpoon')
-            harpoon.setup()
-
-            vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-            vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-            vim.keymap.set('n', '<leader>0', function() harpoon:list():select(1) end)
-            vim.keymap.set('n', '<leader>1', function() harpoon:list():select(2) end)
-            vim.keymap.set('n', '<leader>2', function() harpoon:list():select(3) end)
-            vim.keymap.set('n', '<leader>3', function() harpoon:list():select(4) end)
-            vim.keymap.set('n', '<leader>4', function() harpoon:list():select(5) end)
-        end
-    },
-    { 'j-hui/fidget.nvim', opts = {} },
-    {
-        'folke/tokyonight.nvim',
-        priority = 1000,
-        opts = {
-            styles = { comments = { italic = false }, keywords = { italic = false }},
-            on_colors = function(colors)
-                colors.bg = '#16161e'
-                colors.bg_statusline = '#292e42'
-                colors.bg_float = '#222228'
-            end
-        },
-        init = function() vim.cmd.colorscheme('tokyonight-night') end
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        main = 'nvim-treesitter.configs',
-        opts = {
-            auto_install = true,
-            highlight = { enable = true, additional_vim_regex_highlighting = false }
-        }
-    }
-}, {
-    ui = {
-        icons = {
-            cmd = '⌘', config = '🛠', event = '📅', ft = '📂', init = '⚙',
-            keys = '🗝', plugin = '🔌', runtime = '💻', require = '🌙',
-            source = '📄', start = '🚀', task = '📌', lazy = '💤 '
-        }
+    keymap = {
+        preset = 'default',
+        [ '<C-k>' ] = { 'select_prev', 'fallback' },
+        [ '<C-j>' ] = { 'select_next', 'fallback' }
     }
 })
